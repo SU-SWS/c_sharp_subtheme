b5ef970860b874bb75e597688265f6077ac3f67c
mdyoung3 <mdyoung3@stanford.edu>
2025-06-06 11:30:23 -0600

    CSHARP-64 - reverse order menu

--- decoupled-menu/src/main-menu.island.tsx
+++ decoupled-menu/src/main-menu.island.tsx
@@ -144,7 +144,7 @@ export const MainMenu = ({}) => {
 
     fetch(DRUPAL_DOMAIN + '/jsonapi/menu_items/main')
       .then(res => res.json())
-      .then(data => setMenuItems(deserialize(data)))
+      .then(data => setMenuItems(buildMenuTree(deserialize(data)).items || []))
       .catch(err => console.error(err));
   }, [])
 
@@ -157,8 +157,7 @@ export const MainMenu = ({}) => {
 
   useEventListener("keydown", handleEscape);
 
-  const menuTree = useMemo(() => buildMenuTree(menuItems), [menuItems]);
-  if (!menuTree.items || menuTree.items?.length === 0) return;
+  if (menuItems.length === 0) return;
 
   // Remove the default menu.
   const existingMenu = document.getElementsByClassName('su-multi-menu');
@@ -168,6 +167,7 @@ export const MainMenu = ({}) => {
     <nav
       ref={navRef}
       style={{position: "relative"}}
+      className="preact-main-menu"
     >
       <MobileMenuButton
         ref={buttonRef}
@@ -198,7 +198,7 @@ export const MainMenu = ({}) => {
 
         </SearchContainer>
         <TopList>
-          {menuTree.items.sort((a, b) => a.weight < b.weight ? -1 : 1).map(item => <MenuItem key={item.id} {...item}/>)}
+          {menuItems.map(item => <MenuItem key={item.id} {...item}/>)}
         </TopList>
       </MenuWrapper>
     </nav>
@@ -425,7 +425,7 @@ const MenuItem = ({id, title, url, items, expanded, level = 0}: {
       {(items && expanded) &&
         <MenuList open={submenuOpen} level={level}>
 
-          {items.sort((a, b) => a.weight < b.weight ? -1 : 1).map(item =>
+          {items.map(item =>
             <MenuItem key={item.id} {...item} level={level + 1}/>
           )}
         </MenuList>
